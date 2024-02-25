c342381df0b7f435fecf0cb9931d9657385fc464
Prakruthi52 <prakruthiks52@.com>
2024-02-25 18:19:07 +0530

    feature signup working

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:green_guardians/screens/login.dart';
+import 'package:green_guardians/screens/greenwoodmap.dart';
+import 'package:green_guardians/screens/natureconnect.dart';
+import 'package:green_guardians/screens/reportecocrime.dart';
+import 'package:green_guardians/screens/evergreencanavas.dart';
+import 'package:green_guardians/screens/communityhub.dart';
+import 'package:green_guardians/screens/environmentaleducationpage.dart';
+import 'package:green_guardians/screens/analysis.dart';
+
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +23,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: LoginPage(),
+       routes: {
+        // Define your routes here
+        '/greenwoodmap': (context) => const GreenWoodMapsPage(),
+        '/natureconnect': (context) =>const NatureConnectPage(),
+        '/reportecocrime': (context) => const ReportEcoCrimePage(),
+        '/evergreencanavas': (context) => const EvergreenCanavasPage(), 
+        '/communityhub': (context) => const CommunityHubPage(),
+        '/environmentaleducationpage': (context) => const EnvironmentalEducationPage(),
+        '/analysis': (context) => const AnalysisPage(),
+      },
+      home: const LoginPage() ,
+      debugShowCheckedModeBanner: false ,
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -3,72 +3,77 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:green_guardians/screens/signin.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          const SizedBox(height: 25),
           CachedNetworkImage(
-            imageUrl: 'https://i.pinimg.com/736x/d5/0b/d2/d50bd25b9cc865495b31a97e7a1eed18.jpg',
+            imageUrl: 'https://w0.peakpx.com/wallpaper/479/343/HD-wallpaper-nature-cottage-high-quality-house-landscape-love.jpg',
             placeholder: (context, url) => const CircularProgressIndicator(),
             errorWidget: (context, url, error) => const Text('Error loading image'),
           ),
+          Container(
+            color: Colors.black.withOpacity(0.4), // Overlay with opacity
+          ),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: Container(),
-              ),
-              Text(
-                'Welcome to Emerald Atlas',
+              const Text(
+                'Welcome to',
                 style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24.0,
+                  color: Colors.white,
+                  fontSize: 28.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 10.0),
-              Text(
-                'Step into the heart of nature with our Digital Forest Hub ',
+              const Text(
+                'Emerald Atlas',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              const Text(
+                'Plant your roots deep, where the earth whispers strength, and reach for the sun, where dreams take flight',
+                textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 18.0,
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: Container(),
-              ),
+              const SizedBox(height: 40.0),
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
                 child: Material(
                   elevation: 5.0,
                   borderRadius: BorderRadius.circular(30.0),
-                  color: Colors.white,
+                  color: Colors.green,
                   child: MaterialButton(
                     onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder:(context) => Signin(),));
+                      Navigator.of(context).push(MaterialPageRoute(builder:(context) => const Signin(),));
                       // Implement your login logic
                     },
                     minWidth: double.infinity,
-                    height: 40.0,
-                    child: Text(
-                      'Login',
+                    height: 50.0,
+                    child: const Text(
+                      'Create Account',
                       style: TextStyle(
                         fontSize: 20.0,
-                        color: Colors.green,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                 ),
               ),
-              Expanded(
-                flex: 2,
-                child: Container(),
-              ),
+              const SizedBox(height: 20.0),
             ],
           ),
         ],
